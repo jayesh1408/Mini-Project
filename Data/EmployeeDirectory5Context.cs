9cfe00bbd1eb9cb1392bde8d0ba0cf1e8c4caa1b
Jayesh Patil <jayesh_patil1@persistent.com>
2022-09-14 10:45:53 +0530

    Add project files.

---
+++ Data/EmployeeDirectory5Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeDirectory5.Models;
+
+namespace EmployeeDirectory5.Data
+{
+    public class EmployeeDirectory5Context : DbContext
+    {
+        public EmployeeDirectory5Context (DbContextOptions<EmployeeDirectory5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmployeeDirectory5.Models.Employee> Employee { get; set; } = default!;
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,43 @@
+﻿namespace EmployeeDirectory5.Models
+{
+    public class Employee
+
+    {
+        public int EmployeeId { get; set; }
+
+
+
+
+
+         public String fName { get; set; }
+
+
+
+        public String LName { get; set; }
+        public long PhNo { get; set; }
+
+
+
+        public string grade { get; set; }
+
+
+
+        public String stream { get; set; }
+
+
+
+        public String Technology { get; set; }
+
+
+
+        public String workManager { get; set; }
+
+
+
+        public String LineManager { get; set; }
+
+
+
+        public String AreaOfIntestr { get; set; }
+    }
+}
